a19c397a0d436087a56997c7295d978184f251d4
Hao Chen <hche8093@uni.sydney.edu.au>
2023-05-18 22:49:20 +1000

    Add state with ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client'
+import GuestList from './state/GuestList'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<div>Hi there</div>)
+root.render(
+	<div>
+		<GuestList />
+	</div>
+)
---
+++ src/state/GuestList.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const GuestList: React.FC = () => {
+	const [name, setName] = useState('')
+
+	return (
+		<div>
+			<h3>GuestList</h3>
+			<input
+				type='text'
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<button>Add Guest</button>
+		</div>
+	)
+}
+
+export default GuestList
