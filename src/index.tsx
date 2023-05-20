a9ba45577c8abf51a1e18e230460197bb583131f
Hao Chen <hche8093@uni.sydney.edu.au>
2023-05-20 21:04:06 +1000

    Add UserSearch component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import GuestList from './state/GuestList'
+// import GuestList from './state/GuestList'
+import UserSearch from './state/UserSearch'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<div>
-		<GuestList />
+		{/* <GuestList /> */}
+		<UserSearch />
 	</div>
 )
