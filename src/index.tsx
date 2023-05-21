94bdf215f9639ac29d8d6a8e5aa1855bb4128d5e
Hao Chen <hche8093@uni.sydney.edu.au>
2023-05-21 23:27:49 +1000

    Add useRef

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client'
 // import GuestList from './state/GuestList'
 // import UserSearch from './state/UserSearch'
-import EventComponent from './events/EventComponent'
+// import EventComponent from './events/EventComponent'
+import UserSearch from './refs/UserSearch'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<div>
-		<EventComponent />
+		<UserSearch />
 	</div>
 )
